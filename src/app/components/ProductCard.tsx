90cf6999654fc295fee22404935ff99f70029ce0
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2026-09-17 12:08:01 +0300

    Homepage product list and categories

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { ProductType } from "../types"
+
+const ProductCard = ({product}:{product: ProductType}) => {
+    return (
+        <div className="">
+            ProductCard
+        </div>
+    )
+}
+
+export default ProductCard
